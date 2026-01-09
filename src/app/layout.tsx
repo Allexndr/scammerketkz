a84ddb5e1f8e4054123f24ce8904d2773d88a80e
Aleksandr Murdasov <Allexndr@users.noreply.github.com>
2026-01-09 10:42:51 +0500

    fix: restore root layout and remove redundant default.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+// Since we have a root `[locale]` layout, the root layout is just a shell
+// to satify Next.js requirements. It passes children through.
+export default function RootLayout({ children }: { children: ReactNode }) {
+    return children;
+}
