e8f8a7667790cc556f8f523e0aae6f5685cba682
Aleksandr Murdasov <Allexndr@users.noreply.github.com>
2026-01-09 10:57:30 +0500

    fix: ensure root layout has html and body tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,5 +3,9 @@ import { ReactNode } from 'react';
 // Since we have a root `[locale]` layout, the root layout is just a shell
 // to satify Next.js requirements. It passes children through.
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return children;
+    return (
+        <html>
+            <body>{children}</body>
+        </html>
+    );
 }
